c6d57077feea6302bcecd3993eb9ce2235051564
Kelvin <kxyz5566@gmail.com>
2024-09-02 17:41:17 +0800

    feat: home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  constructor(private dataService: DataService) {
+    if (this.dataService.allLoc.length === 0) {
+      this.dataService.initAllData().subscribe();
+    }
+  }
 
+  get allLoc() {
+    return this.dataService.allLoc;
+  }
 }
