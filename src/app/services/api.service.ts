6ede96b164c3ad31e29db6ac0cbfcf086572f21f
Kelvin <kxyz5566@gmail.com>
2024-09-02 17:16:50 +0800

    feat: Run Chrome browser without CORS

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { appPath } from '../app-path.const';
 import type { DefaultResponse } from '../models/common';
 
+//Run Chrome browser without CORS
+//Add new shortcut, set the target as "[PATH_TO_CHROME]\chrome.exe" --disable-web-security --disable-gpu --user-data-dir=%LOCALAPPDATA%\Google\chromeTemp
 export const apiUrl = 'https://www.travel.taipei/open-api/zh-tw';
 
 @Injectable({
