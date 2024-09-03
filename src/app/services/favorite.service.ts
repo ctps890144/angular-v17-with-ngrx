0ddf03adbb982e4393495bee729dc79c7d46b729
Kelvin <kxyz5566@gmail.com>
2024-09-03 09:45:22 +0800

    feat: FavoriteService

---
+++ src/app/services/favorite.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { shareReplay, Subject } from 'rxjs';
+import type { FavoriteLoc } from '../models/common';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FavoriteService {
+  currFavLoc$: Subject<FavoriteLoc | null> = new Subject();
+  getCurrFavLoc$ = this.currFavLoc$.asObservable().pipe(shareReplay(1));
+
+  constructor() {}
+
+  get storageItem() {
+    return 'favs';
+  }
+
+  setInfo(info: FavoriteLoc[]) {
+    let ciphertext = btoa(JSON.stringify(info));
+    localStorage.setItem(this.storageItem, ciphertext);
+  }
+
+  getInfo(): FavoriteLoc[] {
+    let info: FavoriteLoc[] = [];
+    const storageObj: string | null = localStorage.getItem(this.storageItem);
+
+    if (storageObj !== null && storageObj !== '') {
+      try {
+        info = JSON.parse(atob(storageObj));
+      } catch (error) {
+        info = [];
+      }
+    }
+
+    return info;
+  }
+
+  add(obj: FavoriteLoc) {
+    let info = this.getInfo();
+    let check = info.findIndex((el) => el.id === obj.id);
+
+    if (check >= 0) {
+      info[check] = obj;
+    } else {
+      info.push(obj);
+    }
+
+    this.setInfo(info);
+  }
+
+  remove(id: string) {
+    let info = this.getInfo().filter((el) => el.id !== id);
+
+    this.setInfo(info);
+  }
+
+  removeInfo() {
+    localStorage.removeItem(this.storageItem);
+  }
+}
